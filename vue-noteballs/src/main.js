2c92a3035f055ff2e9d24e878f3bd22231542005
khoabv <khoabv@smartosc.com>
2022-09-06 00:48:14 +0700

    first commit

---
+++ vue-noteballs/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './App.vue'
+
+import './assets/main.css'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
+
+createApp(App).use(router).mount('#app')
